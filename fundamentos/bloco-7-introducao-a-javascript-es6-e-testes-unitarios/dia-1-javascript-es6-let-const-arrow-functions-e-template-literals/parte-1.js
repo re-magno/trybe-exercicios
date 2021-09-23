34610b40062f51d4a2723123f2226522cd51e0fe
Renata <renatamagno@live.com>
2021-09-23 18:19:33 -0300

    Modifica a estrutura da função para que ela seja uma arrow function

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
@@ -3,7 +3,7 @@
 // Modifique a estrutura da função para que ela seja uma arrow function .
 // Modifique as concatenações para template literals . 
 
-function testingScope(escopo) {
+const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
