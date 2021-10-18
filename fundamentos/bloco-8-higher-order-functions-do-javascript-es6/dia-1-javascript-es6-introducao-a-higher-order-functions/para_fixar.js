41760d122a869743b9d5902e3f23c2970cb65460
Renata <renatamagno@live.com>
2021-10-17 21:59:04 -0300

    Para fixar

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/para_fixar.js
@@ -0,0 +1,28 @@
+// Para fixar
+// Vamos praticar com os seguintes exercícios:
+// 1 - Crie uma função que retorne a string 'Acordando!!' ;
+
+const wakeUp = () => 'Acordando!!';
+
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
+
+const breakfast = () => 'Bora tomar café!!';
+
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
+
+const toSleep = () => 'Partiu dormir!!';
+
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores. Exemplo:
+
+const doingThings = (callback) => {
+  const func = callback();
+  console.log(func);
+};
+
+// Ao chamar a função doingThings:
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(toSleep);
+
+// Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// Acordando!!
