020cab11cfcadb1d95380cd26031f4fe600d1e9b
Renata <renatamagno@live.com>
2021-10-29 14:23:15 -0300

    Iniciando exercícios

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercises/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercises/my-pokedex/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
 import pokemons from './data';
+import Pokedex from './Pokedex';
+
 
 function App() {
   return (
-    <></>
+    <>
+    </>
   );
 }
 
