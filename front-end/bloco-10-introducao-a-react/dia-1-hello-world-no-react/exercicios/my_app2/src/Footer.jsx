d02797acc4aa4788f5605c0a0cbcce8a21105c35
Renata <renatamagno@live.com>
2021-11-01 18:44:15 -0300

    Exercicio 3

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios/my_app2/src/Footer.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <h1>"E isso é só o começo..."</h1>
+    );
+  }
+}
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios/my_app2/src/Header.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react'; 
+
+export default class Header extends Component {
+  render() {
+    return (
+      <h1>Conteúdos de Front-End</h1>
+    )
+  }
+}
