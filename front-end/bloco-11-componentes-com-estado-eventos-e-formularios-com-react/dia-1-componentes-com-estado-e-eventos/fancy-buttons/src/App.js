97bdba4305fa21473d56879b2f0bd5a925d626e7
Renata <renatamagno@live.com>
2021-11-03 18:50:19 -0300

    feat: Para fixar - Exercicio 6 - implemente contador nos btns

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -11,26 +11,27 @@ class App extends React.Component {
       btn2: 0,
       btn3: 0,
     };
+
   }
 
   handleClick1() {
-    console.log('Clicou no botão 1!');
+    this.setState((e, _props) => ({btn1: e.btn1 + 1}))
   }
   
   handleClick2() {
-    console.log('Clicou no botão 2!');
+    this.setState((e, _props) => ({btn2: e.btn2 + 1}))
   }
   
   handleClick3() {
-    console.log('Clicou no botão 3!');
+    this.setState((e, _props) => ({btn3: e.btn3 + 1}))
   }
 
   render() {
     return (
     <>
-      <button onClick={ this.handleClick1 }>Botão 1</button>
-      <button onClick={ this.handleClick2 }>Botão 2</button>
-      <button onClick={ this.handleClick3 }>Botão 3</button>
+      <button onClick={ this.handleClick1 }>{this.state.btn1}</button>
+      <button onClick={ this.handleClick2 }>{this.state.btn2}</button>
+      <button onClick={ this.handleClick3 }>{this.state.btn3}</button>
     </>
     );
   }
