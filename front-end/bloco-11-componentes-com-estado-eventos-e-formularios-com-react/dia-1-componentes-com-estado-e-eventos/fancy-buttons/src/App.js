87333612bfb4620ad4816d8f288f8f835b6ca814
Renata <renatamagno@live.com>
2021-11-03 17:32:01 -0300

    feat: Para fixar - Exercicio 2

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-function handleClick() {
-  console.log('Clicou no botão!');
+function handleClick1() {
+  console.log('Clicou no botão 1!');
+}
+
+function handleClick2() {
+  console.log('Clicou no botão 2!');
+}
+
+function handleClick3() {
+  console.log('Clicou no botão 3!');
 }
 
 class App extends React.Component {
   render() {
-    return (<button onClick={ handleClick }>Botão</button>);
+    return (
+      <>
+        <button onClick={ handleClick1 }>Botão 1</button>
+        <button onClick={ handleClick2 }>Botão 2</button>
+        <button onClick={ handleClick3 }>Botão 3</button>
+      </>
+    )
   }
 }
 
