62f603dab294eb3ef429fe1ecad35ce6dc5885c8
Renata <renatamagno@live.com>
2022-01-02 19:50:29 -0300

    exercicio 7

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise7.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise7.test.js
@@ -1,8 +1,14 @@
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
+
 it('uppercase "test" to equal "TEST"', (done) => {
   uppercase('test', (str) => {
     try {
       expect(str).toBe('TEST');
-      done();nm
+      done();
     } catch (error) {
       done(error);
     }
