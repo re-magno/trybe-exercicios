bbbeb8f6f1aec9f4061cf846db28f728b1c2cbc5
Renata <renatamagno@live.com>
2022-03-19 21:13:07 -0300

    :test_tube: exercise 3
    add test case for nan parameter

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests.js
@@ -51,4 +51,36 @@ describe('Executa a função checkNumbSign', () =>{
       });
     });
   });
+
+  describe('se o número for menor que 0', () => {
+    describe ('a resposta', () => {
+      it('é uma string', () => {
+        const response = checkNumbSign(-8);
+
+        expect(response).to.be.a('string');
+      });
+
+      it('é igual a "negativo"', () => {
+        const response = checkNumbSign(-8);
+
+        expect(response).to.be.equals('negativo');
+      });
+    });
+  });
+
+  describe('se o parametro não for um número', () => {
+    describe ('a resposta', () => {
+      it('é uma string', () => {
+        const response = checkNumbSign('abc');
+
+        expect(response).to.be.a('string');
+      });
+
+      it('é igual a "o valor deve ser um número"', () => {
+        const response = checkNumbSign('abc');
+
+        expect(response).to.be.equals('o valor deve ser um número');
+      });
+    });
+  });
 });
